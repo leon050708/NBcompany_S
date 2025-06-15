71854d6ac22e45cf7b6ab86352a57557a2ded1a6
leon <leon050708@outlook.com>
2025-06-15 16:56:19 +0800

    2025061516lhw

---
+++ src/main/java/org/example/nbcompany/NbCompanyApplication.java
@@ -0,0 +1,13 @@
+package org.example.nbcompany;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NbCompanyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NbCompanyApplication.class, args);
+    }
+
+}
